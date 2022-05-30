35929df68830ef2564b05260b04f6c94e3bbd00f
Justin Turkiewicz <justint417@vt.edu>
2022-05-29 22:20:49 -0400

    First commit

---
+++ src/pages/home/home.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './home.scss';
+const Home = () =>  {
+    return (
+        <React.Fragment>
+        </React.Fragment>
+
+    );
+}
+
+export default Home;
