83e9120127a9994860cf25bd206e3aee9ddf4423
= <justin.turkiewicz@gmail.com>
2022-06-30 18:08:40 -0400

    Added a highlight to buttons for tracks

--- src/model/components/tracksComponent/cardComponent/cardComponent.tsx
+++ src/model/components/tracksComponent/cardComponent/cardComponent.tsx
@@ -10,15 +10,29 @@ export class CardComponent extends Component<CardProps>{
     constructor(props: CardProps){
         super(props);
     }
+    handleClick(idName: string){
+        
+       let currentButton = document.getElementById(idName)
+       if(!currentButton?.classList.contains("selected")){
+        currentButton?.classList.add("selected");
+        currentButton?.classList.remove("unselected");
+       }else{
+        currentButton?.classList.remove("selected");
+        setTimeout(() => {
+            currentButton?.classList.add("unselected");
+        }, 1000)
+
+       }
+    }
     createTracksOnPage(){
         let trackCards = [];
         for(let i = 0;i<Object.keys(this.props.tracks!).length;i++) {
             let idName = "trackCard"+i
             // console.log(this.state.songDict[i]);
-            trackCards.push(<div id={idName} key={i}>
-                <div className="songNames">{this.props.tracks![i]}</div>
+            trackCards.push(<button id={idName} key={i} className="unselected" onClick={() => this.handleClick(idName)}>
+                <label className="songNames">{this.props.tracks![i]}</label>
                 {/* <div className="songNumbers">{this.songNumber}</div> */}
-            </div>);
+            </button>);
         }
     // console.log(trackCards);
     return <div id="listOfTrackCards">{trackCards}</div>;
